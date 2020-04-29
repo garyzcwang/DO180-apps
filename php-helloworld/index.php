3e59792aff44da51a5b4b3e6ac8dbee1a6f17240
Student User <student@workstation.lab.example.com>
2020-04-29 09:30:03 +0000

    Update app (2nd).

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming by GW...\n"
+print "Hello, World! php version is " . PHP_VERSION . "<br>\n";
+print "A change is coming by GW...<br>\n"
 ?>
